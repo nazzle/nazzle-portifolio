ae8c47a2a8206ec07ffa8926c79107fddceef50c
Israel Kamendu <israelkamendu@iNazzle.local>
2023-04-21 02:21:54 +0300

    Implemented navs section

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineInfoCircle, AiOutlineContacts } from 'react-icons/ai'
+import { GiNetworkBars } from 'react-icons/gi'
+import { BsListTask } from 'react-icons/bs'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" className='active'><AiOutlineHome /></a>
+      <a href="#about"><AiOutlineInfoCircle /></a>
+      <a href="#experience"><GiNetworkBars /></a>
+      <a href="#services"><BsListTask /></a>
+      <a href="#contact"><AiOutlineContacts /></a>
+    </nav>
   )
 }
 
