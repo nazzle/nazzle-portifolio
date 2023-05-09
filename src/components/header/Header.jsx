7ecf94b29a7ab9ec4afd1f33ad25214081196c20
israel <israel.kamendu@tcra.go.tz>
2023-05-09 18:37:18 +0300

    changed to nazzle space namespace

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/images/nazzleTech.png'
+import ME from '../../assets/images/nspace.png'
 import SocialMedia from './SocialMedia'
 
 const Header = () => {
